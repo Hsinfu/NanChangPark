cc7b027a03d26863d9c9a47baa5f7edd36a2bf3d
hsinfu <hsinfu8055@gmail.com>
2019-04-11 15:05:51 +0800

    add pygame Keyboard

--- src/game.py
+++ src/game.py
@@ -4,6 +4,8 @@ import time
 import pygame as pg
 import pandas as pd
 
+from keyboard import Keyboard
+
 from constant import (
     GAME_RECORDS_PATH,
     PLAYERS_DIR,
@@ -40,31 +42,20 @@ def load_imgs(dir_name, count, size=None):
         return os.path.join(dir_name, '{:05d}.png'.format(i))
     return [load_img(get_fname(i)) for i in range(count)]
 
-imgs = {
-    'bg': load_imgs('bg', 48),
-    'bar': load_imgs('bar', 48),
-    # 'welcome': load_img('welcome.png'),
-    # 'scan': load_img('scan.png'),
-    # 'confirm': load_img('confirm.png'),
-    # 'level1-description': load_img('level1-description.png'),
-    # 'level1-map': load_img('level1-description.png'),
-}
+# imgs = {
+#     'bg': load_imgs('bg', 48),
+#     'bar': load_imgs('bar', 48),
+#     # 'welcome': load_img('welcome.png'),
+#     # 'scan': load_img('scan.png'),
+#     # 'confirm': load_img('confirm.png'),
+#     # 'level1-description': load_img('level1-description.png'),
+#     # 'level1-map': load_img('level1-description.png'),
+# }
 
 
 class Level:
     def __init__(self):
         pass
-
-
-class Stage:
-    def __init__(self):
-        pass
-
-
-class WelcomeStage(Stage):
-    def __init__(self):
-        pass
-
 
 
 class Frame:
@@ -86,19 +77,48 @@ class Frame:
         self.move()
 
 
+
+class Stage:
+    def __init__(self):
+        pass
+
+
+class WelcomeStage(Stage):
+    def __init__(self):
+        self.welcome_frames = Frame([load_img('welcome.png')])
+
+
+
 class Game:
     def __init__(self, player_name):
-        self.bg_frames = Frame(imgs['bg'])
-        self.bar_frames = Frame(imgs['bar'])
+        self.bg_frames = Frame(load_imgs('bg', 48))
+        self.bar_frames = Frame(load_imgs('bar', 48))
         self._scores = starting_scores
+        self.state = 'welcome'
+        self.stages = {
+            'welcome': WelcomeStage(),
+        }
 
-    def tick(self, events):
-        logger.info('Game tick')
+    @property
+    def stage(self):
+        return self.stages[self.state]
+
+    @property
+    def is_playing_stage(self):
+        return 'level' in self.state
+
+    def draw(self):
         surface_flags = pg.HWACCEL | pg.HWSURFACE
         sf = pg.Surface(screen.get_size(), flags=surface_flags).convert()
         self.bg_frames.draw(sf)
-        self.bar_frames.draw(sf)
+        if not self.is_playing_stage:
+            self.bar_frames.draw(sf)
         screen.blit(sf, (0, 0))
+
+    def tick(self, keyboard):
+        logger.info('Game tick')
+        self.draw()
+        # self.stage
         return None
 
 
@@ -114,6 +134,7 @@ class RankGame:
             ]
         """
         self._game = None
+        self._keyboard = None
         self.records_path = records_path
         try:
             self.records = pd.read_json(records_path)
@@ -134,26 +155,35 @@ class RankGame:
             self._game = Game(self.player_name)
         return self._game
 
+    @property
+    def keyboard(self):
+        if self._keyboard is None:
+            self._keyboard = Keyboard()
+        return self._keyboard
+
     def add(self, record):
         self.records.append(pd.DataFrame([record]), ignore_index=True)
         self.records.to_json(self.records_path, orient='records')
         self._game = None
 
-    def tick(self, events):
+    def tick(self, keyboard):
         # logger.info('RankGame tick')
-        record = self.game.tick(events)
+        record = self.game.tick(keyboard)
         if record is not None:
             self.add(record)
 
     def start(self):
         while True:
             events = pg.event.get()
-            # print(events)
+            logger.debug('events: {}'.format(events))
+            self.keyboard.update(events)
+            logger.debug('keyboard: {}'.format(self.keyboard.keys))
+
             if any([e.type == pg.QUIT for e in events]):
                 break
-            if any([e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE for e in events]):
+            if self.keyboard.is_pressed(pg.K_ESCAPE):
                 break
-            self.tick(events)
+            self.tick(self.keyboard)
             pg.display.flip()
             clock.tick(frame_rate)
 
---
+++ src/keyboard.py
@@ -0,0 +1,38 @@
+import pygame as pg
+
+class Keyboard:
+    def __init__(self):
+        self.keys = {
+            pg.K_ESCAPE: False,
+            pg.K_LEFT: False,
+            pg.K_RIGHT: False,
+            pg.K_DOWN: False,
+            pg.K_UP: False,
+            pg.K_a: False,
+            pg.K_b: False,
+        }
+
+    def press(self, key):
+        try:
+            self.keys[key] = True
+        except KeyError:
+            pass
+
+    def release(self, key):
+        try:
+            self.keys[key] = False
+        except KeyError:
+            pass
+
+    def update(self, events):
+        for e in events:
+            if e.type == pg.KEYDOWN:
+                self.press(e.key)
+            elif e.type == pg.KEYUP:
+                self.release(e.key)
+
+    def is_pressed(self, key):
+        try:
+            return self.keys[key]
+        except KeyError:
+            return False
